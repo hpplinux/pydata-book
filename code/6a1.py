63e61325cb147b75e7e16754053aea44019e1e06
hpp <pengpeng@iscas.ac.cn>
2016-08-16 09:10:31 +0800

    update

--- code/6a1.py
+++ code/6a1.py
@@ -1,6 +1,21 @@
+#coding:utf-8
 import pandas as pd
 
-df=pd.read_csv('/root/pydata-book/ch06/ex1.csv')
+import csv
+
+csvfile=file('csvtest.csv','wb')
+writer=csv.writer(csvfile)
+
+writer.writerow(['姓名','年龄','电话'])
+
+data=[
+	('小河','25','123456'),
+	('小芳','23','1254354')
+]
+
+writer.writerows(data)
+
+csvfile.close()
 
 
 
